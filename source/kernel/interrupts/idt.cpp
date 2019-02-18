e177039e3f70cda42eb75c18a61f87d0103a9d4b
Daouki <daouki97@gmail.com>
2019-02-18 14:26:48 +0100

    Fix IDT.

--- source/kernel/interrupts/idt.cpp
+++ source/kernel/interrupts/idt.cpp
@@ -4,9 +4,6 @@
 
 #define IDT_ENTRY_COUNT 256
 
-#define GATE_TYPE_INTERRUPT
-#define GATE_TYPE_TRAP
-
 struct IDTEntry {
     uint16_t address_low;  // Bits 0..15 of the handler function address.
     uint16_t selector;     // Code segment selector defined by the GDT.
@@ -17,17 +14,17 @@ struct IDTEntry {
     uint32_t padding;      // Always zero.
 };
 
-static_assert(sizeof(IDTEntry) == 16);
-
-static struct IDT { IDTEntry entries[IDT_ENTRY_COUNT]; } g_idt = {{0, 0, 0, 0, 0, 0, 0}};
-
-static_assert(sizeof(IDT) == sizeof(IDTEntry) * IDT_ENTRY_COUNT);
+struct IDT {
+    IDTEntry entries[IDT_ENTRY_COUNT];
+};
 
 struct IDTPtr {
     uint16_t size;
     uint64_t address;
 } __attribute__((packed));
 
+static_assert(sizeof(IDTEntry) == 16);
+static_assert(sizeof(IDT) == sizeof(IDTEntry) * IDT_ENTRY_COUNT);
 static_assert(sizeof(IDTPtr) == 10);
 
 extern "C" void interrupt_division_error(void);
@@ -46,19 +43,26 @@ extern "C" void interrupt_gpf(void);
 extern "C" void interrupt_page_fault(void);
 extern "C" void interrupt_alignment_check(void);
 
+IDT    g_idt;
+IDTPtr g_idtr;
+
 static void register_interrupt_handler(
         const uint32_t index, void (*handler)(), uint8_t ist, uint8_t attributes) {
-    const uint64_t address           = reinterpret_cast<uint64_t>(handler);
-    g_idt.entries[index].address_low = address & 0xFFFF;
-    g_idt.entries[index].selector    = 0x28;
-    g_idt.entries[index].ist         = ist;
-    g_idt.entries[index].attributes  = attributes;
-    g_idt.entries[index].address_mid = (address & 0xFFFF0000) >> 16;
-    g_idt.entries[index].address_low = (address & 0xFFFFFFFF00000000) >> 32;
-    g_idt.entries[index].padding     = 0;
+    const uint64_t address            = reinterpret_cast<uint64_t>(handler);
+    g_idt.entries[index].address_low  = address & 0xFFFF;
+    g_idt.entries[index].selector     = 0x28;
+    g_idt.entries[index].ist          = ist;
+    g_idt.entries[index].attributes   = attributes;
+    g_idt.entries[index].address_mid  = (address & 0xFFFF0000) >> 16;
+    g_idt.entries[index].address_high = (address & 0xFFFFFFFF00000000) >> 32;
+    g_idt.entries[index].padding      = 0;
 }
 
 void initialize_idt() {
+    for (uint32_t i = 0; i < IDT_ENTRY_COUNT; i += 1) {
+        register_interrupt_handler(i, interrupt_division_error, 0, 0x8E);
+    }
+
     register_interrupt_handler(0x00, interrupt_division_error, 0, 0x8E);
     register_interrupt_handler(0x01, interrupt_single_step, 0, 0x8E);
     register_interrupt_handler(0x02, interrupt_nmi, 0, 0x8E);
@@ -74,7 +78,8 @@ void initialize_idt() {
     register_interrupt_handler(0x0D, interrupt_gpf, 0, 0x8E);
     register_interrupt_handler(0x0E, interrupt_page_fault, 0, 0x8E);
     register_interrupt_handler(0x11, interrupt_alignment_check, 0, 0x8E);
-    IDTPtr idt_ptr{sizeof(IDTEntry) * IDT_ENTRY_COUNT - 1, reinterpret_cast<uint64_t>(&g_idt)};
-    asm volatile("lidt %0" ::"m"(idt_ptr));
-    asm volatile("sti");
+
+    g_idtr.size = sizeof(IDTEntry) * IDT_ENTRY_COUNT - 1;
+    g_idtr.address = reinterpret_cast<uint64_t>(&g_idt.entries);
+    asm volatile("lidt %0" ::"m"(g_idtr));
 }
